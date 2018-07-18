cd2443a09a9bf318e3ac71b5144b76e7d19bc29b
Neal Morton <mortonne@gmail.com>
2018-07-17 22:54:52 -0500

    option to pass in sem_vec also (or instead) of sem_mat

--- util/trim_sem_mat.m
+++ util/trim_sem_mat.m
@@ -1,5 +1,5 @@
-function [pres_itemnos_trim, sem_mat_trim] = trim_sem_mat(pres_itemnos, ...
-                                                  sem_mat)
+function [pres_itemnos_trim, sem_mat_trim, sem_vec_trim] = trim_sem_mat(pres_itemnos, ...
+                                                  sem_mat, sem_vec)
 %TRIM_SEM_MAT   Remove unused items from a semantic matrix.
 %
 %  Can be used to speed up execution of simulations that vary with the
@@ -9,9 +9,23 @@ function [pres_itemnos_trim, sem_mat_trim] = trim_sem_mat(pres_itemnos, ...
 %
 %  [pres_itemnos_trim, sem_mat_trim] = trim_sem_mat(pres_itemnos, sem_mat)
 
+if nargin < 3
+    sem_vec = [];
+end
+
 [itemnos, ia, ic] = unique(pres_itemnos);
 n_item = length(itemnos);
 new_itemnos = [1:n_item]';
 pres_itemnos_trim = reshape(new_itemnos(ic), size(pres_itemnos));
 
-sem_mat_trim = sem_mat(itemnos, itemnos);
+if ~isempty(sem_mat)
+    sem_mat_trim = sem_mat(itemnos, itemnos);
+else
+    sem_mat_trim = [];
+end
+
+if ~isempty(sem_vec)
+    sem_vec_trim = sem_vec(:,itemnos);
+else
+    sem_vec_trim = [];
+end
