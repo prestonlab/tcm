3b20d40bc113911bc3fdb2a5df10ce0581db0db0
Neal Morton <mortonne@gmail.com>
2018-07-22 12:23:11 -0500

    add aperture external directory to path

--- init_tcm.m
+++ init_tcm.m
@@ -13,6 +13,10 @@ addpath(fullfile(proj_dir, 'tcm', 'tests'))
 addpath(fullfile(proj_dir, 'src'))
 addpath(fullfile(proj_dir, 'util'))
 
+% external
+addpath(fullfile(proj_dir, 'external', 'mvpa'))
+addpath(fullfile(proj_dir, 'external', 'aperture'))
+
 % experiments
 addpath(fullfile(proj_dir, 'exp', 'cfrl'))
 addpath(fullfile(proj_dir, 'exp', 'cfrl', 'data'))
