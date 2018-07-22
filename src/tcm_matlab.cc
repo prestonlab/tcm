2de57b7c71dfb14d2afe617e29b08cff0c081cc4
Neal Morton <mortonne@gmail.com>
2018-07-22 04:13:34 -0500

    new interface for distcon with item semantic cuing

--- src/tcm_matlab.cc
+++ src/tcm_matlab.cc
@@ -155,6 +155,80 @@ double run_tcm_distcon (double *r_mat, double *param_mat, unsigned int R,
   return logl;
 }
 
+double run_tcm_distcon_sem (double *r_mat, double *param_mat, unsigned int R, 
+			    unsigned int P, unsigned int N,
+			    double *itemno_mat, double *sem_vec,
+			    double *sem_mat, unsigned int n_list,
+			    unsigned int sem_rows,
+			    unsigned int sem_cols) {
+  vector<double> param_vec (P);
+  vector<unsigned int> recalls (R);
+  vector< vector<unsigned int> > pres_itemno (n_list, vector<unsigned int> (N));
+  vector< vector<double> > semvec (sem_rows, vector<double> (sem_cols));
+  vector< vector<double> > sem (sem_cols, vector<double> (sem_cols));
+  double logl = 0;
+  bool isdc = true;
+  
+  // read parameters into a Parameters object
+  unsigned int i;
+  for (i = 0; i < P; ++i) {
+    param_vec[i] = param_mat[i];
+  }
+  Parameters param (param_vec);
+
+  // copy recall codes into a vector
+  for (i = 0; i < R; ++i) {
+    recalls[i] = r_mat[i];
+  }
+  
+  // copy item numbers to a vector of vectors
+  unsigned int j;
+  unsigned int n = 0;
+  unsigned int max_itemno = 0;
+  for (j = 0; j < N; ++j) {
+    for (i = 0; i < n_list; ++i) {
+      pres_itemno[i][j] = static_cast<unsigned int>(itemno_mat[n]);
+      max_itemno = max(max_itemno, pres_itemno[i][j]);
+      n++;
+    }
+  }
+  if (max_itemno > sem_cols) {
+    mexErrMsgIdAndTxt("MATLAB:run_tcm_distcon:itemnoVecMismatch",
+		      "Some items do not have corresponding vectors.");
+  }
+
+  // copy semantic similarities to a vector of vectors
+  n = 0;
+  for (j = 0; j < sem_cols; ++j) {
+    for (i = 0; i < sem_rows; ++i) {
+      semvec[i][j] = sem_vec[n];
+      n++;
+    }
+  }
+
+  // copy semantic similarities to a vector of vectors
+  n = 0;
+  for (i = 0; i < sem_cols; ++i) {
+    for (j = 0; j < sem_cols; ++j) {
+      sem[i][j] = sem_mat[n];
+      n++;
+    }
+  }
+
+  // create recalls object for the task
+  Recall rec (N, sem_rows, isdc, param, recalls);
+
+  // attached semantic similarity values to the network
+  rec.setPoolVec(&pres_itemno, &semvec);
+  rec.setPoolSim(&pres_itemno, &sem);
+  
+  // evaluate free recall likelihood
+  rec.task();
+  logl = rec.logL();
+  
+  return logl;
+}
+
 /* The gateway function */
 void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   double * r_mat; // recalls vector
@@ -189,10 +263,17 @@ void mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
       // use semantic similarity to affect cuing in the model
       logl = run_tcm_sem(r_mat, param_mat, R, P, N, itemno_mat, sem_mat,
      			 n_list, sem_rows);
-    } else {
+    } else if (sem_type == 2) {
       // use semantic feature vectors to drive context evolution
       logl = run_tcm_distcon(r_mat, param_mat, R, P, N, itemno_mat, sem_mat,
   			     n_list, sem_rows, sem_cols);
+    } else {
+      double * sem_mat2 = mxGetPr(prhs[6]);
+      unsigned int sem_rows2 = mxGetM(prhs[6]);
+      unsigned int sem_cols2 = mxGetN(prhs[6]);
+      logl = run_tcm_distcon_sem(r_mat, param_mat, R, P, N, itemno_mat,
+				 sem_mat, sem_mat2, n_list,
+				 sem_rows, sem_cols);
     }
   }
   plhs[0] = mxCreateDoubleScalar(logl);
