d5e79c0f2ac7b8d63a43c094601c42d32b459455
Neal Morton <mortonne@gmail.com>
2018-07-23 20:50:38 -0500

    developing parameters searches

--- exp/cfrl/run_tcm_cfrl.m
+++ exp/cfrl/run_tcm_cfrl.m
@@ -8,11 +8,19 @@ addpath ~/matlab/accre
 
 fits = {'base', 'wikiw2v_context' 'wikiw2v_item' 'wikiw2v_context_item'};
 
-fits = {'dc_wikiw2v' 'dc_ncf_wikiw2v'};
+% test job
+fits = {'base'};
 experiments = {'cfr'};
+flags = '-t 00:30:00 --mem=8gb --cpus-per-task=8 --partition=debug';
+jobs = {};
+for i = 1:2
+    jobs{i} = submit_searches_cfrl(experiments, fits, flags, 'n_workers', 8);
+end
 
-flags = '-t 04:00:00 --mem=8gb --cpus-per-task=12';
-n_rep = 10;
+% full search (based on JML paper search parameters)
+fits = {'full_wikiw2v'};
+flags = '-t 24:00:00 --mem=12gb --cpus-per-task=12';
+n_rep = 15;
 jobs = cell(1, n_rep);
 for i = 1:n_rep
     jobs{i} = submit_searches_cfrl(experiments, fits, flags, 'n_workers', 12);
