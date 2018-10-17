5ef2a4edfb0f11f4e9179143a316295b438c85c6
Neal Morton <mortonne@gmail.com>
2018-10-17 15:55:12 -0500

    remove subfunction that is now in project

--- exp/cfrl/prep_data_cfr.m
+++ exp/cfrl/prep_data_cfr.m
@@ -76,34 +76,3 @@ sem_file = fullfile(data_dir, 'cfr_wikiw2v.mat');
 sem_mat = squareform(rescale(squareform(sem_mat)));
 vectors = zscore(vectors, [], 2);
 save(sem_file, 'sem_mat', 'rdm', 'vectors', 'items');
-
-
-function x = rescale(x)
-%RESCALE   Rescale data to range from 0 to 1.
-%
-%  y = rescale(x)
-%
-%  INPUTS:
-%        x:  an [observations X variables] matrix of data. Each column
-%            will be rescaled separately.
-%
-%  OUTPUTS:
-%        y:  rescaled data.
-
-% input checks
-if ~exist('x', 'var')
-    error('You must pass a matrix.')
-elseif ~isnumeric(x)
-    error('Data must be numeric.')
-end
-
-% rescale
-x_min = min(x, [], 1);
-x_range = range(x, 1);
-for i=1:size(x, 2)
-    if x_range(i) == 0
-        x(:,i) = 1;
-    else
-        x(:,i) = (x(:,i) - x_min(i)) / x_range(i);
-    end
-end
