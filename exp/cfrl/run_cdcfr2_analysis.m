9e758853953e2fb75839326c2b88a6120f8f549c
Neal Morton <mortonne@gmail.com>
2018-10-17 16:24:47 -0500

    code to load data and create semantic bins

---
+++ exp/cfrl/run_cdcfr2_analysis.m
@@ -0,0 +1,8 @@
+
+%% load basic data
+
+files = get_exp_info_cfrl('cdcfr2');
+real = load(files.data);
+sem = load(files.wikiw2v.raw);
+
+create_sem_crp_bins(real.data, sem.sem_mat, files.wikiw2v.bin);
