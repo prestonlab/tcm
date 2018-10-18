f8e1793691b0d730861d59ec00783299f9c4f397
Neal Morton <mortonne@gmail.com>
2018-10-18 14:51:53 -0500

    fixes for saving searches

--- exp/cfrl/save_search_cfrl.m
+++ exp/cfrl/save_search_cfrl.m
@@ -4,9 +4,13 @@ function [res_file, stats] = save_search_cfrl(res, experiment, fit)
 %  [res_file, stats] = save_search_cfrl(res, experiment, fit)
 
 % get search output file
-model_type = res(1).model_type;
+model_type = res(1).fstruct.model_type;
 timestamp = datestr(now, 29);
 filename = sprintf('%s_%s.mat', model_type, timestamp);
+info = get_fit_info_cfrl(fit, experiment);
+if ~exist(info.res_dir, 'dir')
+    mkdir(info.res_dir);
+end
 res_file = get_next_file(fullfile(info.res_dir, filename));
 
 % unpack search results into full format
