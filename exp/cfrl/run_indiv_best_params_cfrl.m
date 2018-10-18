ae92fbca13a7d3d435f5996e499cd9a4f6b48f37
Neal Morton <mortonne@gmail.com>
2018-10-18 14:08:44 -0500

    change to use prep_param_cfrl

--- exp/cfrl/run_indiv_best_params_cfrl.m
+++ exp/cfrl/run_indiv_best_params_cfrl.m
@@ -22,28 +22,8 @@ stats = getfield(load(info.res_file, 'stats'), 'stats');
 % load data
 simdef = sim_def_cfrl(experiment, fit);
 data = getfield(load(simdef.data_file, 'data'), 'data');
-if simdef.opt.qsem
-    custom.sem_mat = getfield(load(simdef.sem_mat_file, 'sem_mat'), 'sem_mat');
-end
-
-% define vector components
-if simdef.opt.dc
-    custom.sem_vec = getfield(load(simdef.sem_mat_file, 'vectors'), 'vectors')';
-end
-n_item = length(unique(data.pres_itemnos));
-if simdef.opt.loc
-    custom.loc_vec = eye(n_item);
-end
-if simdef.opt.cat
-    [~, ind] = unique(data.pres_itemnos);
-    category = data.pres.category(ind);
-    ucat = unique(category);
-    cat_vec = zeros(length(ucat), n_item);
-    for i = 1:length(ucat)
-        cat_vec(i,category==ucat(i)) = 1;
-    end
-    custom.cat_vec = cat_vec;
-end
+pool = load(simdef.pool_file);
+custom = prep_param_cfrl(custom, simdef, pool.category);
 
 subject = unique(data.subject);
 n_subj = length(subject);
