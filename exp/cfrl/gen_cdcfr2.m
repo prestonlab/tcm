950548ad2ca290921083f8846be51107fd48a94f
Neal Morton <mortonne@gmail.com>
2018-10-22 16:17:51 -0500

    change to use split_distract_cfrl

--- exp/cfrl/gen_cdcfr2.m
+++ exp/cfrl/gen_cdcfr2.m
@@ -3,26 +3,22 @@ function seq = gen_cdcfr2(param, data, n_rep)
 %
 %  seq = gen_cdcfr2(param, data, n_rep)
 
-% original and replicated distraction labels
-distract = data.pres.distractor(:,1);
-udistract = unique(distract);
-rep_distract = repmat(distract, [n_rep 1]);
+[n_trials, n_items, n_recalls] = size_frdata(data);
+seq = NaN(n_trials * n_rep, n_items);
 
-% generate sequences for all distraction conditions
-[n_list, n_item] = size(data.pres_itemnos);
-seq = NaN(n_list * n_rep, n_item);
-for i = 1:length(udistract)
-    % get data for this subset of trials
-    include = distract==udistract(i);
-    d = struct;
-    d.recalls = data.recalls(include,:);
-    d.pres_itemnos = data.pres_itemnos(include,:);
-    
-    % simulate this distraction condition
-    d_param = param_cdcfr2(param, udistract(i));
-    d_seq = gen_tcm(d_param, d, n_rep);
-    seq(rep_distract==udistract(i),1:size(d_seq, 2)) = d_seq;
+if ~isfield(data, 'distract')
+    data.distract = split_distract_cfrl(data);
 end
 
+% generate sequences for all distraction conditions
+for i = 1:length(data.distract)
+    % get data for this subset of trials
+    d = data.distract{i};
+    d_param = param_cdcfr2(param, d.distract_len);
+    d_seq = gen_tcm(d_param, d, n_rep);
+    seq(d.trials,1:size(d_seq, 2)) = d_seq;
+end
+
+% remove padding
 cols = ~all(seq==0, 1);
 seq = seq(:,cols);
