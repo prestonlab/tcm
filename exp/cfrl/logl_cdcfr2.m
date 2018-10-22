f54da67f8c8b810d5ef1c6d3d352ad580c5dd91c
Neal Morton <mortonne@gmail.com>
2018-10-22 13:03:19 -0500

    change to call param_cdcfr2

--- exp/cfrl/logl_cdcfr2.m
+++ exp/cfrl/logl_cdcfr2.m
@@ -6,16 +6,7 @@ function logl = logl_cdcfr2(param, data)
 % run separately for each distraction condition
 logl = 0;
 for i = 1:length(data.distract)
-    distract_len = data.distract{i}.distract_len;
-    if distract_len == 2.5
-        param.B_ipi = param.B_ipi1;
-        param.B_ri = param.B_ri1;
-        param.X2 = param.X21;
-    elseif distract_len == 7.5
-        param.B_ipi = param.B_ipi2;
-        param.B_ri = param.B_ri2;
-        param.X2 = param.X22;
-    end
-    logl_distract = logl_mex_tcm(param, data.distract{i});
+    d_param = param_cdcfr2(param, data.distract{i}.distract_len);
+    logl_distract = logl_mex_tcm(d_param, data.distract{i});
     logl = logl + logl_distract;
 end
