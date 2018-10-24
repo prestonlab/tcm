33658daa366381af16da2fbb7bd1cb756b53d413
Neal Morton <mortonne@gmail.com>
2018-10-24 17:55:45 -0500

    fix for new prep_param_cfrl version

--- exp/cfrl/indiv_context_cfrl.m
+++ exp/cfrl/indiv_context_cfrl.m
@@ -3,7 +3,7 @@ function [subj_data, subj_param, c_pres, c_rec] = indiv_context_cfrl(stats, simd
 %
 %  [subj_data, c_pres, c_rec] = indiv_context_cfrl(stats, simdef)
 
-load(simdef.data_file)
+pool = load(simdef.pool_file);
 subj_data = cell(1, length(stats));
 subj_param = cell(1, length(stats));
 c_pres = cell(1, length(stats));
@@ -12,7 +12,7 @@ for i = 1:length(stats)
     % construct parameters from the best-fitting vector
     param = unpack_param(stats(i).parameters, stats(i).param_info);
     param = propval(simdef.fixed, param, 'strict', false);
-    param = prep_param_cfrl(param, simdef, data);
+    param = prep_param_cfrl(param, simdef, pool.category);
     param = check_param_cfrl(param);
 
     % enable context recording
