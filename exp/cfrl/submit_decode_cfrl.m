2ad0b7131c9deb48cbd8e18441e8c384246ec28c
Neal Morton <mortonne@gmail.com>
2019-01-01 15:13:32 -0600

    script to submit parallel jobs for decoding analysis

---
+++ exp/cfrl/submit_decode_cfrl.m
@@ -0,0 +1,26 @@
+function job = submit_decode_cfrl(experiment, fit, res_name, w, flags)
+%SUBMIT_DECODE_CFRL   Submit jobs to run decoding analysis in parallel.
+%
+%  job = submit_decode_cfrl(experiment, fit, res_name, w, flags)
+
+info = get_fit_info_cfrl('local_cat_wikiw2v', 'cfr');
+[par, base, ext] = fileparts(info.res_file);
+
+s = load(info.res_file);
+n_subj = length(s.stats);
+
+cluster = parallel.cluster.Generic();
+cluster.JobStorageLocation = '~/runs';
+cluster.IntegrationScriptsLocation = '~/matlab/accre';
+cluster.AdditionalProperties.AdditionalSubmitArgs = [' ' flags];
+
+job = createJob(cluster);
+for i = 1:n_subj
+    outfile = fullfile(par, sprintf('%s_%s_%d.mat', base, res_name, i));
+    if ~exist(outfile, 'file')
+        task = createTask(job, @decode_cfrl, 1, {experiment, fit, res_name, ...
+                            w, 'subj_ind', i, 'overwrite', true});
+    end
+end
+
+submit(job);
