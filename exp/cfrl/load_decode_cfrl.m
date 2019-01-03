6b30774b25cb979e854974b465b407c0062f23fc
Neal Morton <mortonne@gmail.com>
2019-01-03 14:53:36 -0600

    add warnings about missing fields

--- exp/cfrl/load_decode_cfrl.m
+++ exp/cfrl/load_decode_cfrl.m
@@ -9,13 +9,28 @@ info = get_fit_info_cfrl('local_cat_wikiw2v', 'cfr');
 s = load(info.res_file);
 n_subj = length(s.stats);
 
+f_all = {'subj_data' 'subj_param' 'c' 'c_in' 'ic' 'pat_file' ...
+         'eeg_evidence_raw' 'eeg_perf' 'eeg_evidence' 'con_evidence_raw' ...
+         'con_evidence' 'con_perf_raw'};
+
 for i = 1:n_subj
     outfile = fullfile(par, sprintf('%s_%s_%d.mat', base, res_name, i));
     if i == 1
         stats = load(outfile);
         f = fieldnames(stats);
+        missing = setdiff(f_all, f);
+        if ~isempty(missing)
+            fprintf('subject %d missing fields:\n', i);
+            disp(missing)
+        end
     else
         s = load(outfile);
+        missing = setdiff(f_all, fieldnames(s));
+        if ~isempty(missing)
+            fprintf('subject %d missing fields:\n', i);
+            disp(missing)
+        end
+        
         for j = 1:length(f)
             if ~isfield(s, f{j})
                 fprintf('problem with subject index %d.\n', i);
