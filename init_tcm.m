0da78efbaac7324994b1bf05e6aab9b3d507101d
Neal Morton <mortonne@gmail.com>
2020-03-19 14:09:00 -0500

    remove external dependencies and experiments from init script

--- init_tcm.m
+++ init_tcm.m
@@ -13,10 +13,5 @@ addpath(fullfile(proj_dir, 'tcm', 'tests'))
 addpath(fullfile(proj_dir, 'src'))
 addpath(fullfile(proj_dir, 'util'))
 
-% external
-addpath(fullfile(proj_dir, 'external', 'mvpa'))
-addpath(fullfile(proj_dir, 'external', 'aperture'))
-
-% experiments
-addpath(fullfile(proj_dir, 'exp', 'cfrl'))
-addpath(fullfile(proj_dir, 'exp', 'cfrl', 'data'))
+% sample data
+addpath(fullfile(proj_dir, 'data'))
