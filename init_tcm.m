26c0d886ee181270d6a77e59063dc1083361265e
Neal Morton <mortonne@gmail.com>
2020-03-19 14:15:47 -0500

    move library directories to lib

--- init_tcm.m
+++ init_tcm.m
@@ -6,12 +6,14 @@ function init_tcm()
 proj_dir = fileparts(mfilename('fullpath'));
 
 % main directories
-addpath(fullfile(proj_dir, 'parallel'))
-addpath(fullfile(proj_dir, 'search'))
-addpath(fullfile(proj_dir, 'tcm'))
-addpath(fullfile(proj_dir, 'tcm', 'tests'))
+addpath(fullfile(proj_dir, 'lib', 'parallel'))
+addpath(fullfile(proj_dir, 'lib', 'search'))
+addpath(fullfile(proj_dir, 'lib', 'tcm'))
+addpath(fullfile(proj_dir, 'lib', 'tcm', 'tests'))
+addpath(fullfile(proj_dir, 'lib', 'util'))
+
+% compiled binary files
 addpath(fullfile(proj_dir, 'src'))
-addpath(fullfile(proj_dir, 'util'))
 
 % sample data
 addpath(fullfile(proj_dir, 'data'))
